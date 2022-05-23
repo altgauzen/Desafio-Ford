08dd107aba7fd3b9e59e4682662c35e218551835
Alexandre Altgauzen <alexandrealtgauzen@Alexandres-MacBook-Air.local>
2022-05-23 15:54:29 +0100

    refact: clean no used code.

--- cripto.js
+++ cripto.js
@@ -6,32 +6,13 @@ const vinString = "1HGCM82633A004352";
 const arrayVin = vinString.split("");
 console.log("arrayVIN", arrayVin);
 
-
 let array = [];
 
 for (let l = 0; l < arrayVin.length; l++) {
-       array[l] = parseInt(arrayVin[l].charCodeAt(0)) + 3;
-      }
-
-// infelizmente meu mac deu pau e me atrapalhei com o tempo.
+  array[l] = parseInt(arrayVin[l].charCodeAt(0)) + 3;
+};
 
 console.log(array);
 newArray = [];
 
-// for (let m = 0; m < array.length; m++) {
-//   if ()
-//   newArray[l] = parseInt(arrayVin[l].charCodeAt(0)) + 3;
-//  }
-
-
-// array[0].map(x => String.fromCharCode(x))
-
-
-// let charCodeArray = firstArray.map(x => x.charCodeAt(0));
-// console.log(charCodeArray);
-
-// for (let j = len/2; j < len; j++) {
-//   reversed[j] -= 1;
-// }
-
-// let encript = reversed.map(x => String.fromCharCode(x))
+// infelizmente meu mac deu pau e me atrapalhei com o tempo.
